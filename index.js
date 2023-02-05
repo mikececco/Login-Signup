5d845caf939eba331705cb1398ed71ad583a702c
mikececco <mikececco2000@gmail.com>
2023-02-05 17:34:42 +0100

    Added error behaviour on Username input field in Create account page

--- index.js
+++ index.js
@@ -7,14 +7,16 @@ function setFormMessage(formElement, type, message){
 
 }
 
-function setInputError(formElement, type, message){
-    const messageElement = formElement.querySelector(".form_message")
-
-    messageElement.textContent = message;
-    messageElement.classList.remove("form_message_error", "form_message_success");
-    messageElement.classList.add("form_message_${type}");
-
+function setInputError(inputElement, message){
+    inputElement.classList.remove("form_input_error");
+    inputElement.parentElement.querySelector(".form_input_error_message").textContent = message;
 }
+
+function clearInputError(inputElement){
+    inputElement.classList.add("form_input_error");
+    inputElement.parentElement.querySelector(".form_input_error_message").textContent = "";
+}
+
 
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -37,6 +39,18 @@ document.addEventListener("DOMContentLoaded", () => {
         e.preventDefault();
 
         // add behaviour, either error or success message by passing setFormMessage function
-    })
+    });
+
+    document.querySelectorAll(".form_input").forEach(inputElement => {
+        inputElement.addEventListener("blur", e => {
+            if (e.target.id === "signupUsername" && e.target.value.length > 0 && e.target.value.length < 10) {
+                setInputError(inputElement, "Username must be at least 10 characters in length");
+            }
+        });
+
+        inputElement.addEventListener("input", e => {
+            clearInputError(inputElement);
+        });
+    });
 
 });
